8038a90e772d8cd3755ac5f87193190a5a02b159
Alex Aranda <alexraranda@gmail.com>
2021-02-08 22:13:21 -0800

    unique userName, create useCreateRecipe hook
    Create hook for CreateRecipe page
    
    Add useCreateRecipe

--- src/Routes/CreateRecipe.tsx
+++ src/Routes/CreateRecipe.tsx
@@ -1,8 +1,9 @@
-import firebase from "firebase";
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement } from "react";
 import { useSelector } from "react-redux";
-import { useHistory, useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
+import { AddRecipeInput, AddRecipeTextArea } from "../components/Input";
+import { useCreateRecipe } from "../hooks/useCreateRecipe";
 import { RootState } from "../reducers/rootReducer";
 import { styles } from "../styles/styles";
 
@@ -32,34 +33,6 @@ const SubContainer = styled.div<IProps>`
   float: ${(props) => props.float};
   height: fit-content;
   @media screen and (max-width: ${mobileMaxWidth}) {
-    width: 100%;
-  }
-`;
-
-const Item = styled.div<{ height?: string; width?: string }>`
-  width: 60%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: start;
-  margin-bottom: 20px;
-  & > textarea {
-    font-size: 1rem;
-    line-height: 1.5rem;
-    height: ${(props) => props.height};
-    width: 100%;
-    font-family: ${primaryFont};
-    font-weight: 600;
-    outline: none;
-  }
-  & > input {
-    font-size: 1rem;
-    line-height: 1.5rem;
-    height: 2.5rem;
-    width: ${(props) => props.width};
-    outline: none;
-  }
-  & > label {
-    margin: 5px 0;
     width: 100%;
   }
 `;
@@ -94,222 +67,24 @@ const ButtonContainer = styled.div`
 `;
 
 const CreateRecipe = (): ReactElement => {
-  const [recipeName, setRecipeName] = useState("");
-  const [description, setDescription] = useState("");
-  const [ingredients, setIngredients] = useState("");
-  const [instructions, setInstructions] = useState("");
-  const [category, setCategory] = useState("");
-  const [servings, setServings] = useState(0);
-  const [image, setImage] = useState<Blob | Uint8Array | ArrayBuffer | null>(
-    null,
-  );
-  const [imagePreview, setImagePreview] = useState(
-    "https://firebasestorage.googleapis.com/v0/b/my-recipes-da233.appspot.com/o/fr9urcWIV8V2IXoWgBTwMLIgVW02%2FToast-1612819853305?alt=media&token=8346302d-aeb6-4ca8-a3df-163d262657d5",
-  );
-  const [imageToUpdate, setImageToUpdate] = useState("");
-  const { uid } = useSelector((state: RootState) => state.userReducer);
+  const { inputs, setInputs, submit, edit } = useCreateRecipe();
   const { recipes } = useSelector((state: RootState) => state.recipeReducer);
   const location = useLocation();
   const { id } = useParams<{ id: string }>();
-  const history = useHistory();
-  const db = firebase.database();
-  const storageRef = firebase
-    .storage()
-    .ref()
-    .child(`${uid}/${recipeName}-${Date.now()}`);
-
-  const changeRecipeName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRecipeName(e.currentTarget.value);
-  };
-
-  const changeDescription = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setDescription(e.currentTarget.value);
-  };
-
-  const changeIngredients = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setIngredients(e.currentTarget.value);
-  };
-
-  const changeInstructions = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInstructions(e.currentTarget.value);
-  };
-
-  const changeCategory = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCategory(e.currentTarget.value);
-  };
-
-  const changeServings = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const num = parseInt(e.currentTarget.value);
-    setServings(num);
-  };
-
-  const submitNewRecipe = () => {
-    if (
-      imagePreview ===
-      "https://firebasestorage.googleapis.com/v0/b/my-recipes-da233.appspot.com/o/fr9urcWIV8V2IXoWgBTwMLIgVW02%2FToast-1612819853305?alt=media&token=8346302d-aeb6-4ca8-a3df-163d262657d5"
-    ) {
-      const key = db.ref().child("recipes").push().key;
-      const recipe = {
-        createdBy: uid,
-        name: recipeName,
-        description,
-        category,
-        servings,
-        ingredients,
-        instructions,
-        image: imagePreview,
-      };
-      db.ref(`recipes/${key}`).set(recipe);
-      history.push(`/user/${uid}`);
-    } else if (image) {
-      const task = storageRef.put(image);
-      task.on(
-        "state_changed",
-        (snapshot) => {
-          console.log(snapshot);
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          task.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-            const key = db.ref().child("recipes").push().key;
-            const recipe = {
-              createdBy: uid,
-              name: recipeName,
-              description,
-              category,
-              servings,
-              ingredients,
-              instructions,
-              image: downloadURL,
-            };
-            db.ref(`recipes/${key}`).set(recipe);
-            history.push(`/user/${uid}`);
-          });
-        },
-      );
-    }
-  };
-
-  const editRecipe = () => {
-    if (image) {
-      const task = storageRef.put(image);
-      task.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          task.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-            const recipe = {
-              createdBy: uid,
-              name: recipeName,
-              description,
-              category,
-              servings,
-              ingredients,
-              instructions,
-              image: downloadURL,
-            };
-            db.ref(`recipes/${id}`).set(recipe);
-            history.push(`/recipedetail/${id}`);
-          });
-        },
-      );
-      return null;
-    }
-    const recipe = {
-      createdBy: uid,
-      name: recipeName,
-      description,
-      category,
-      servings,
-      ingredients,
-      instructions,
-    };
-    db.ref("recipes/").child(id).update(recipe);
-    history.push(`/recipedetail/${id}`);
-  };
 
   const handleSubmitOrEditButtons = () => {
     return recipes && location.pathname === `/editrecipe/${id}` ? (
-      <button onClick={() => editRecipe()}>Edit Recipe</button>
+      <button onClick={() => edit()}>Edit Recipe</button>
     ) : (
-      <button onClick={() => submitNewRecipe()}>Save Recipe</button>
+      <button onClick={() => submit()}>Save Recipe</button>
     );
-  };
-
-  const handleInputItem = (
-    name: string,
-    value: string | number,
-    cb: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    width: string,
-    type?: string,
-  ) => {
-    return (
-      <Item width={width}>
-        <label htmlFor={name}>{name}</label>
-        <input type={type} value={value} onChange={cb} />
-      </Item>
-    );
-  };
-
-  const handleTextItem = (
-    name: string,
-    value: string,
-    cb: (e: React.ChangeEvent<HTMLTextAreaElement>) => void,
-    placeholder: string,
-    height: string,
-  ) => {
-    return (
-      <Item height={height}>
-        <label htmlFor={name}>{name}</label>
-        <textarea value={value} onChange={cb} placeholder={placeholder} />
-      </Item>
-    );
-  };
-
-  useEffect(() => {
-    if (recipes && location.pathname === `/editrecipe/${id}`) {
-      const {
-        name,
-        description,
-        ingredients,
-        instructions,
-        servings,
-        category,
-        image,
-      } = recipes[id];
-      setRecipeName(name);
-      setDescription(description);
-      setIngredients(ingredients);
-      setInstructions(instructions);
-      setServings(servings);
-      setCategory(category);
-      setImageToUpdate(image);
-    }
-  }, [id, location.pathname, recipes]);
-
-  const changeImage = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const { files } = e.target;
-    if (files && files.length) {
-      setImagePreview(URL.createObjectURL(files[0]));
-      setImage(files[0]);
-      setImageToUpdate("");
-    }
   };
 
   const renderImage = () => {
     return (
       <img
         style={{ height: "175px", width: "175px" }}
-        src={imageToUpdate ? imageToUpdate : imagePreview}
+        src={inputs.imageToUpdate ? inputs.imageToUpdate : inputs.imagePreview}
       />
     );
   };
@@ -318,49 +93,67 @@ const CreateRecipe = (): ReactElement => {
     <>
       <ContainerDiv>
         <SubContainer width="60%" float="right">
-          {handleInputItem(
-            "Recipe Title",
-            recipeName,
-            changeRecipeName,
-            "100%",
-          )}
-          {handleTextItem(
-            "Description",
-            description,
-            changeDescription,
-            "",
-            "6rem",
-          )}
-          {handleTextItem(
-            "Ingredients",
-            ingredients,
-            changeIngredients,
-            "Write each ingredient on its own line",
-            "9rem",
-          )}
-          {handleTextItem(
-            "Instructions",
-            instructions,
-            changeInstructions,
-            "Write each instruction on its own line",
-            "9rem",
-          )}
+          <AddRecipeInput
+            error=""
+            name="Recipe Title"
+            type="text"
+            placeholder="Enter Recipe Title"
+            value={inputs.recipeName}
+            onChange={(e) => setInputs.setRecipeName(e.target.value)}
+            width="100%"
+          />
+          <AddRecipeTextArea
+            error=""
+            name="Description"
+            placeholder="Enter Description"
+            value={inputs.description}
+            onChange={(e) => setInputs.setDescription(e.target.value)}
+            height="6rem"
+          />
+          <AddRecipeTextArea
+            error=""
+            name="Ingredients"
+            placeholder="Write each ingredient on its own line"
+            value={inputs.ingredients}
+            onChange={(e) => setInputs.setIngredients(e.target.value)}
+            height="9rem"
+          />
+          <AddRecipeTextArea
+            error=""
+            name="Instructions"
+            placeholder="Write each instruction on its own line"
+            value={inputs.instructions}
+            onChange={(e) => setInputs.setInstructions(e.target.value)}
+            height="9rem"
+          />
         </SubContainer>
         <SubContainer width="40%" float="left">
           <FileDiv>
             <FileLabel>
               {renderImage()}
-              <input type="file" onChange={changeImage} />
+              <input type="file" onChange={setInputs.setImage} />
             </FileLabel>
           </FileDiv>
-          {handleInputItem("Category", category, changeCategory, "100%")}
-          {handleInputItem(
-            "Servings",
-            servings,
-            changeServings,
-            "50%",
-            "number",
-          )}
+          <AddRecipeInput
+            error=""
+            name="Category"
+            type="text"
+            placeholder="Enter Category"
+            value={inputs.category}
+            onChange={(e) => setInputs.setCategory(e.target.value)}
+            width="100%"
+          />
+          <AddRecipeInput
+            error=""
+            name="Servings"
+            type="number"
+            placeholder=""
+            value={inputs.servings}
+            onChange={(e) =>
+              setInputs.setServings(parseInt(e.currentTarget.value))
+            }
+            width="50%"
+          />
         </SubContainer>
         <ButtonContainer>{handleSubmitOrEditButtons()}</ButtonContainer>
       </ContainerDiv>
---
+++ src/hooks/useCreateRecipe.ts
@@ -0,0 +1,177 @@
+import firebase from "firebase";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useHistory, useLocation, useParams } from "react-router-dom";
+import { RootState } from "../reducers/rootReducer";
+
+export const useCreateRecipe = () => {
+  const defaultImage =
+    "https://firebasestorage.googleapis.com/v0/b/my-recipes-da233.appspot.com/o/fr9urcWIV8V2IXoWgBTwMLIgVW02%2FToast-1612819853305?alt=media&token=8346302d-aeb6-4ca8-a3df-163d262657d5";
+  const [recipeName, setRecipeName] = useState("");
+  const [description, setDescription] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [category, setCategory] = useState("");
+  const [servings, setServings] = useState(0);
+  const [image, setImage] = useState<Blob | Uint8Array | ArrayBuffer | null>(
+    null,
+  );
+  const [imagePreview, setImagePreview] = useState(defaultImage);
+  const [imageToUpdate, setImageToUpdate] = useState("");
+
+  const { uid } = useSelector((state: RootState) => state.userReducer);
+  const { recipes } = useSelector((state: RootState) => state.recipeReducer);
+  const location = useLocation();
+  const { id } = useParams<{ id: string }>();
+  const history = useHistory();
+  const db = firebase.database();
+  const storageRef = firebase
+    .storage()
+    .ref()
+    .child(`${uid}/${recipeName}-${Date.now()}`);
+
+  const submitNewRecipe = () => {
+    if (imagePreview === defaultImage) {
+      const key = db.ref().child("recipes").push().key;
+      const recipe = {
+        createdBy: uid,
+        name: recipeName,
+        description,
+        category,
+        servings,
+        ingredients,
+        instructions,
+        image: imagePreview,
+      };
+      db.ref(`recipes/${key}`).set(recipe);
+      history.push(`/user/${uid}`);
+    } else if (image) {
+      const task = storageRef.put(image);
+      task.on(
+        "state_changed",
+        (snapshot) => {
+          console.log(snapshot);
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          task.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+            const key = db.ref().child("recipes").push().key;
+            const recipe = {
+              createdBy: uid,
+              name: recipeName,
+              description,
+              category,
+              servings,
+              ingredients,
+              instructions,
+              image: downloadURL,
+            };
+            db.ref(`recipes/${key}`).set(recipe);
+            history.push(`/user/${uid}`);
+          });
+        },
+      );
+    }
+  };
+
+  const editRecipe = () => {
+    if (image) {
+      const task = storageRef.put(image);
+      task.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          task.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+            const recipe = {
+              createdBy: uid,
+              name: recipeName,
+              description,
+              category,
+              servings,
+              ingredients,
+              instructions,
+              image: downloadURL,
+            };
+            db.ref(`recipes/${id}`).set(recipe);
+            history.push(`/recipedetail/${id}`);
+          });
+        },
+      );
+      return null;
+    }
+    const recipe = {
+      createdBy: uid,
+      name: recipeName,
+      description,
+      category,
+      servings,
+      ingredients,
+      instructions,
+    };
+    db.ref("recipes/").child(id).update(recipe);
+    history.push(`/recipedetail/${id}`);
+  };
+
+  useEffect(() => {
+    if (recipes && location.pathname === `/editrecipe/${id}`) {
+      const {
+        name,
+        description,
+        ingredients,
+        instructions,
+        servings,
+        category,
+        image,
+      } = recipes[id];
+      setRecipeName(name);
+      setDescription(description);
+      setIngredients(ingredients);
+      setInstructions(instructions);
+      setServings(servings);
+      setCategory(category);
+      setImageToUpdate(image);
+    }
+  }, [id, location.pathname, recipes]);
+
+  const changeImage = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { files } = e.target;
+    if (files && files.length) {
+      setImagePreview(URL.createObjectURL(files[0]));
+      setImage(files[0]);
+      setImageToUpdate("");
+    }
+  };
+
+  return {
+    inputs: {
+      recipeName,
+      description,
+      ingredients,
+      instructions,
+      category,
+      servings,
+      imagePreview,
+      imageToUpdate,
+    },
+    setInputs: {
+      setRecipeName,
+      setDescription,
+      setIngredients,
+      setInstructions,
+      setCategory,
+      setServings,
+      setImage: changeImage,
+    },
+    submit: submitNewRecipe,
+    edit: editRecipe,
+  };
+};
